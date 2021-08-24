626b76105ac52c1e9b6afcda52178c7c05b8b16a
pedrojus <p.jusino@hotmail.com>
2021-08-24 00:17:20 -0500

    - Created a function called calculate_all_error() where it iterates through a list of data points and calculates the total error for each point. - Switched single line comments to multi-line comments using """

--- main.py
+++ main.py
@@ -15,12 +15,39 @@ def calculate_error(m, b, point):
     diff = result - y_point
     return abs(diff)
 
-
+"""
 # This is a line that looks like y = x, so (3, 3) should lie on it. thus, error should be 0:
-# print(calculate_error(1, 0, (3, 3)))
+print(calculate_error(1, 0, (3, 3)))
 # The point (3, 4) should be 1 unit away from the line y = x:
-# print(calculate_error(1, 0, (3, 4)))
+print(calculate_error(1, 0, (3, 4)))
 # The point (3, 3) should be 1 unit away from the line y = x - 1:
-# print(calculate_error(1, -1, (3, 3)))
+print(calculate_error(1, -1, (3, 3)))
 # The point (3, 3) should be 5 units away from the line y = -x + 1:
-# print(calculate_error(-1, 1, (3, 3)))
+print(calculate_error(-1, 1, (3, 3)))
+"""
+
+# This function iterates through a list of datapoints and calculates the error from that point to the line
+def calculate_all_error(m, b, points):
+    total_error = 0
+    for point in points:
+        total_error += calculate_error(m, b, point)
+    return total_error
+
+""" # every point in this dataset lies upon y=x, so the total error should be zero:
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(1, 0, datapoints))
+
+#every point in this dataset is 1 unit away from y = x + 1, so the total error should be 4:
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(1, 1, datapoints))
+
+#every point in this dataset is 1 unit away from y = x - 1, so the total error should be 4:
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(1, -1, datapoints))
+
+
+#the points in this dataset are 1, 5, 9, and 3 units away from y = -x + 1, respectively, so total error should be
+# 1 + 5 + 9 + 3 = 18
+datapoints = [(1, 1), (3, 3), (5, 5), (-1, -1)]
+print(calculate_all_error(-1, 1, datapoints))
+"""
