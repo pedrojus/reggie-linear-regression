8ba5f7e87ef243d984a0bce6036e29c97f5eba2d
pedrojus <p.jusino@hotmail.com>
2021-08-23 22:45:13 -0500

    - Commented out print statements that tested get_y() - Created a new function called calculate_error() that calculates the error between a point and a line - Added/commented out print statements that tested calculate_error()

--- main.py
+++ main.py
@@ -3,5 +3,24 @@ def get_y(m, b, x):
     return m * x + b
 
 
-print(get_y(1, 0, 7) == 7)
-print(get_y(5, 10, 3) == 25)
+# Print statements to test functionality of get_y function
+# print(get_y(1, 0, 7) == 7)
+# print(get_y(5, 10, 3) == 25)
+
+# This function calculates the error between a point and a line
+def calculate_error(m, b, point):
+    x_point = point[0]
+    y_point = point[1]
+    result = get_y(m, b, x_point)
+    diff = result - y_point
+    return abs(diff)
+
+
+# This is a line that looks like y = x, so (3, 3) should lie on it. thus, error should be 0:
+# print(calculate_error(1, 0, (3, 3)))
+# The point (3, 4) should be 1 unit away from the line y = x:
+# print(calculate_error(1, 0, (3, 4)))
+# The point (3, 3) should be 1 unit away from the line y = x - 1:
+# print(calculate_error(1, -1, (3, 3)))
+# The point (3, 3) should be 5 units away from the line y = -x + 1:
+# print(calculate_error(-1, 1, (3, 3)))
